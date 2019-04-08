fc0bbf41ff5ad04f2b04260deb34c267ec2f71a7
Gill Abada <gill@callifi.com>
2019-04-08 18:16:47 -0400

    succed completed.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,11 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement > 19) {
+    return { ...item };
+  } else {
+    return { ...item, enhancement: item.enhancement + 1 };
+  }
 }
 
 function fail(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,11 +1,5 @@
-const { repair } = require('./enhancer.js');
+const { repair, succeed } = require('./enhancer.js');
 // test away!
-
-const completeItem = {
-  name: 'Test Item',
-  durability: 75,
-  enhancement: 2
-};
 
 const brokenItem = {
   name: 'Test Item',
@@ -13,13 +7,42 @@ const brokenItem = {
   enhancement: 2
 };
 
+const unbrokenItem = {
+  name: 'Test Item',
+  durability: 100,
+  enhancement: 2
+};
+
+const enhancedMaxItem = {
+  name: 'Enhanced Test Item',
+  durability: 50,
+  enhancement: 20
+};
+
+const enhancedItem = {
+  name: 'Enhanced Test Item',
+  durability: 50,
+  enhancement: 10
+};
+
 describe('enhancer', () => {
   describe('repair', () => {
     it('is item repaird', () => {
       expect(repair(brokenItem).durability).toEqual(100);
+      expect(repair(unbrokenItem).durability).toEqual(100);
     });
   });
-  describe('get', () => {
+  describe('succeed', () => {
+    it('check that enhancement level is < 20', () => {
+      expect(succeed(enhancedItem).enhancement).toBeLessThanOrEqual(20);
+      expect(succeed(enhancedMaxItem).enhancement).toBeLessThanOrEqual(20);
+    });
+    it('check that durability is not changed', () => {
+      expect(succeed(enhancedItem).durability).toEqual(enhancedItem.durability);
+      expect(succeed(enhancedMaxItem).durability).toEqual(
+        enhancedItem.durability
+      );
+    });
     it.todo('check that item is an object');
     it.todo('check that item have name, durability, and enhancement');
   });
